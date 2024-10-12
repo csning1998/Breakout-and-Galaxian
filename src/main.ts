e4581ed88c13935cff15689733a83afaed8a8d24
ChingSan Ning <january20070120@gmail.com>
2024-10-13 00:58:07 +0800

    Add the package

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import './assets/GoogleMaterial.css'
+// import 'material-design-icons/iconfont/material-icons.css';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+
+export default {
+    components: {
+        FontAwesomeIcon
+    },
+}
 
 const app = createApp(App)
 
