32496fcbb0ee86ddb349835b60409d2ee3405d75
ChingSan Ning <january20070120@gmail.com>
2024-10-13 00:57:28 +0800

    Fix: env import error

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,10 @@
 ///<reference path=""
 
-import { Ball } from "@/game/components/ball.js";
-import { Paddle } from "@/game/components/paddle.js";
-import { Bricks } from "@/game/components/bricks.js";
+import { Ball } from "@/tsscripts/game/components/ball.js";
+import { Paddle } from "@/tsscripts/game/components/paddle.js";
+import { Bricks } from "@/tsscripts/game/components/bricks.js";
 import { InputHandler } from "./input.js";
-import { GameSettings } from "@/game/shared/settings.js";
+import { GameSettings } from "@/tsscripts/game/shared/settings.js";
 
 declare global {
   interface Scene {
@@ -17,6 +17,17 @@ declare global {
     settings: GameSettings;
     inputHandler: InputHandler;
   }
+}
+
+interface ImportMetaEnv {
+  readonly VITE_YOUR_URL: string;
+  readonly VITE_REALM: string;
+  readonly VITE_CLIENT_ID: string;
+
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
 }
 
 // Ref: https://stackoverflow.com/questions/42233987/how-to-configure-custom-global-interfaces-d-ts-files-for-typescript
